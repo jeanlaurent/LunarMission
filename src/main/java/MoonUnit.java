f49bbb7a1f25c07dcf857a3f3d70e391b397a588
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2014-02-12 12:05:14 +0100

    Stronger test (refactoring)

--- src/main/java/MoonUnit.java
+++ src/main/java/MoonUnit.java
@@ -3,8 +3,12 @@ public class MoonUnit {
 
     private double altitude = 80;
     private double verticalSpeed = 0;
+    private double horizontalSpeed = 0;
     private double direction = 90;
     private boolean motor = false;
+    private int jauge = 20;
+
+    public MoonUnit() {}
 
     public MoonUnit(double startingAltitude) {
         this.altitude = startingAltitude;
@@ -15,7 +19,7 @@ public class MoonUnit {
     }
 
     public double getHorizontalSpeed() {
-        return 0;
+        return horizontalSpeed;
     }
 
     public double getVerticalSpeed() {
@@ -23,7 +27,11 @@ public class MoonUnit {
     }
 
     public double getJauge() {
-        return 20;
+        return this.jauge;
+    }
+
+    public boolean isMotor() {
+        return motor;
     }
 
     public void tick() {
@@ -41,5 +49,39 @@ public class MoonUnit {
 
     public void stopMotor() {
         motor = false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MoonUnit moonUnit = (MoonUnit) o;
+
+        if (Double.compare(moonUnit.altitude, altitude) != 0) return false;
+        if (Double.compare(moonUnit.direction, direction) != 0) return false;
+        if (Double.compare(moonUnit.horizontalSpeed, horizontalSpeed) != 0) return false;
+        if (jauge != moonUnit.jauge) return false;
+        if (motor != moonUnit.motor) return false;
+        if (Double.compare(moonUnit.verticalSpeed, verticalSpeed) != 0) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(altitude);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(verticalSpeed);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(horizontalSpeed);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(direction);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (motor ? 1 : 0);
+        result = 31 * result + jauge;
+        return result;
     }
 }
--- src/test/java/InterfaceTest.java
+++ src/test/java/InterfaceTest.java
@@ -1,27 +1,25 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.xml.ws.Response;
 import java.io.IOException;
 
-import static com.jayway.restassured.RestAssured.get;
 import static com.jayway.restassured.RestAssured.given;
+import static org.fest.assertions.Assertions.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
 
 public class InterfaceTest {
-
-    private static final String JSONDATA = loadJson("moonunit.json");
-    private static final String JSONDATA_AFTER_TICK = loadJson("moonunit-tick.json");
-    private static final String JSONDATAWITHSTART = loadJson("moonunit-start.json");;
     private Server server;
+    private MoonUnit referenceMoonUnit;
 
     @Before
     public void init() {
         server = new Server();
         server.start();
+        referenceMoonUnit = new MoonUnit();
     }
 
     @After
@@ -31,26 +29,42 @@ public class InterfaceTest {
 
     @Test
     public void should_call_server_and_get_moonunit_characteristics() {
-        given().port(server.getPort()).get("/moonunit").then().body(containsString(JSONDATA));
+        MoonUnit moonUnit = getRemoteMoonUnit("/moonunit");
+        assertThat(moonUnit).isEqualTo(referenceMoonUnit);
+    }
+
+    private MoonUnit getRemoteMoonUnit(String url) {
+        return getMoonUnitOutOfJson(given().port(server.getPort()).get(url).body().asString());
     }
 
     @Test
     public void should_call_server_and_trigger_a_tick() {
-        given().port(server.getPort()).get("/moonunit/tick").then().body(containsString(JSONDATA_AFTER_TICK));
+        MoonUnit moonUnit = getRemoteMoonUnit("/moonunit/tick");
+        assertThat(moonUnit.getAltitude()).isLessThan(referenceMoonUnit.getAltitude());
     }
 
     @Test
     public void should_reinit_moonunit_position() {
-        given().port(server.getPort()).get("/moonunit/tick");
+        given().port(server.getPort()).get("/moonunit/tick").then().statusCode(200);
         given().port(server.getPort()).get("/moonunit/reinit").then().statusCode(200);
-        given().port(server.getPort()).get("/moonunit").then().body(containsString(JSONDATA));
+        MoonUnit moonUnit = getRemoteMoonUnit("/moonunit");
+        assertThat(moonUnit).isEqualTo(referenceMoonUnit);
     }
 
     @Test
     public void should_startMotor() {
-        given().port(server.getPort()).get("/moonunit/start");
-        given().port(server.getPort()).get("/moonunit/tick");
-        given().port(server.getPort()).get("/moonunit").then().body(containsString(JSONDATAWITHSTART));
+        given().port(server.getPort()).get("/moonunit/start").then().statusCode(200);
+        given().port(server.getPort()).get("/moonunit/tick").then().statusCode(200);
+        MoonUnit moonUnit = getRemoteMoonUnit("/moonunit");
+        assertThat(moonUnit.getAltitude()).isGreaterThan(referenceMoonUnit.getAltitude());
+    }
+
+    private MoonUnit getMoonUnitOutOfJson(String jsondata) {
+        try {
+            return new ObjectMapper().readValue(jsondata, MoonUnit.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
